a924dffa81624d47310794357a5cb75893909c55
Alamin <alamin017514@gmail.com>
2023-04-06 09:39:04 +0600

    show the order summary

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
+
 
 const Orders = () => {
+    const cart = useLoaderData()
+
     return (
         <div>
-            <h3>This is order page</h3>
+            <div className="shop-container">
+                <div className="cart-container">
+                    <h3>This is order page:{cart.length}</h3>
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Shop from './components/Shop/Shop';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
+import cartProductsLoader from './Loaders/cartProductsLoader';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
       {
         path: 'orders',
         element: <Orders />,
+        loader: cartProductsLoader
       },
       {
         path: 'inventory',
